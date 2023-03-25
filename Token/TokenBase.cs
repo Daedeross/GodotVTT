460a5e94e7b82144b6e9445252cf64323370fb3e
Bryan C. Jones <daedeross@gmail.com>
2023-03-25 12:20:37 -0500

    Trying Godot v3.

--- Token/TokenBase.cs
+++ Token/TokenBase.cs
@@ -3,57 +3,57 @@ using System;
 
 namespace GodotVTT
 {
-    public partial class TokenBase : Area2D
-    {
-        private bool _isMouseOver;
-        private ControlState _controlState = ControlState.None;
+	public partial class TokenBase : Area2D
+	{
+		private bool _isMouseOver;
+		private ControlState _controlState = ControlState.None;
 
-        private MapBase _currentMap;
+		private MapBase _currentMap;
 
-        // Called when the node enters the scene tree for the first time.
-        public override void _Ready()
-        {
-            MouseEntered += OnMouseEntered;
-            MouseExited += OnMouseExited;
-            _currentMap = GetParent<MapBase>();
-        }
+		// Called when the node enters the scene tree for the first time.
+		public override void _Ready()
+		{
+			MouseEntered += OnMouseEntered;
+			MouseExited += OnMouseExited;
+			_currentMap = GetParent<MapBase>();
+		}
 
-        // Called every frame. 'delta' is the elapsed time since the previous frame.
-        public override void _Process(double delta)
-        {
-            if (_controlState == ControlState.Primary)
-            {
-                Position = _currentMap.GetLocalMousePosition();
-            }
-        }
+		// Called every frame. 'delta' is the elapsed time since the previous frame.
+		public override void _Process(double delta)
+		{
+			if (_controlState == ControlState.Primary)
+			{
+				Position = _currentMap.GetLocalMousePosition();
+			}
+		}
 
-        public override void _UnhandledInput(InputEvent @event)
-        {
-            if (_isMouseOver && @event is InputEventMouseButton eventMouseButton)
-            {
-                if (eventMouseButton.IsActionPressed(Actions.Primary))
-                {
-                    _controlState = _controlState | ControlState.Primary;
-                }
-                else if (eventMouseButton.IsActionReleased(Actions.Primary))
-                {
-                    _controlState = _controlState & (~ControlState.Primary);
-                }
-            }
-        }
+		public override void _UnhandledInput(InputEvent @event)
+		{
+			if (_isMouseOver && @event is InputEventMouseButton eventMouseButton)
+			{
+				if (eventMouseButton.IsActionPressed(Actions.Primary))
+				{
+					_controlState = _controlState | ControlState.Primary;
+				}
+				else if (eventMouseButton.IsActionReleased(Actions.Primary))
+				{
+					_controlState = _controlState & (~ControlState.Primary);
+				}
+			}
+		}
 
-        public void RevealFog()
-        {
-        }
+		public void RevealFog()
+		{
+		}
 
-        private void OnMouseEntered()
-        {
-            _isMouseOver = true;
-        }
+		private void OnMouseEntered()
+		{
+			_isMouseOver = true;
+		}
 
-        private void OnMouseExited()
-        {
-            _isMouseOver = false;
-        }
-    }
+		private void OnMouseExited()
+		{
+			_isMouseOver = false;
+		}
+	}
 }

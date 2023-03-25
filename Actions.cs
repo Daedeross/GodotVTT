bd1a4e03f33acc86190c5ef1ed1222fa8996107e
Bryan C. Jones <daedeross@gmail.com>
2023-03-25 11:40:52 -0500

    Basic but buggy VBL working.

---
+++ Actions.cs
@@ -0,0 +1,8 @@
+namespace GodotVTT
+{
+    public static class Actions
+    {
+        public static string Primary { get; } = "primary";
+        public static string Secondary { get; } = "secondary";
+    }
+}
